d32152b680e29011c6115f5ddc0c74dbe9942482
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-02 11:46:55 -0600

    ejercicio 2

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 let hola = document.getElementById('hola');
-hola.innerHTML = 'Hola Bienvenido a la manipulacion del DOM';
+hola.innerHTML = `
+  <h1>Hola Bienvenido a la manipulacion del DOM</h1>
+  <p>Holi</p>
+`;
+
+// let p1 = document.createElement('p');
+// let texto = document.createTextNode('holi')
+// p1.appendChild(texto)
+// document.getElementById('hola').appendChild(p1)
+
+// let boton1 = document.getElementById('boton1')
+
+// boton1.addEventListener('click', (ev) => {
+//   console.log('evento', ev)
+// })
+
+// function myFunction() {
+//   console.log('evento')
+// }
+function agregaEncabezado() {
+  document.getElementById('encabezado').innerHTML = '<h1>Encabezado</h1>';
+}
+
+let btnParrafo = document.getElementById('boton2')
+btnParrafo.addEventListener('click', function () {
+  document.getElementById('parrafo').innerHTML = '<p>Parrafo</p>';
+})
+
+
+function mensaje() {
+  let mensaje = prompt('Teclea el mensaje a visualizar.');
+  alert(mensaje);
+}
+
+
+
+
