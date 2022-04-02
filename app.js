f22418c9db66b07b7f4705e9ba86d801c6800a9f
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-02 12:29:59 -0600

    ejercicio 3

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 let hola = document.getElementById('hola');
 hola.innerHTML = 'Hola Bienvenido a la manipulacion del DOM';
+
+
+let fondo = document.getElementById("primer1");
+
+fondo.addEventListener("click", myFondo);
+
+function myFondo() {
+
+  let element = document.getElementById("resultado");
+
+  element.style.background = "#F8360D";
+
+}
+
+let tamanio = document.getElementById("segund2");
+
+tamanio.addEventListener("click", myTamanio);
+
+function myTamanio() {
+  let element = document.getElementById("resultado");
+  element.style.fontSize = "50px";
+}
+
+let border = document.getElementById('tercer3')
+
+border.addEventListener('click', () => {
+  let element = document.getElementById('resultado')
+  element.style.border = '3px solid blue'
+})
