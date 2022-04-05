9c3d634b5cbe2c7aac6a0eb1d3f9283f32118d73
Alejandro Quiñonez <alquinonez.dwfc5@ucamp.utel.edu.mx>
2022-04-04 20:47:28 -0600

    ejercicio realizado

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 
 
-/*
+/* *
 
 A partir de la página web proporcionada y utilizando las funciones DOM, mostrar en pantalla la siguiente información:
 
-Número de enlaces de la página
-Dirección a la que enlaza el penúltimo enlace
-Numero de enlaces que enlazan a http://prueba
+* Número de enlaces de la página
+* Dirección a la que enlaza el penúltimo enlace
+* Numero de enlaces que enlazan a http://prueba
 Número de enlaces del tercer párrafo
 
 */
 
-console.log(document.getElementsByTagName('a'))
 let enlaces = document.getElementsByTagName('a').length;
-document.getElementById('num-enlaces').innerHTML = enlaces;
-
-let penultimo = document.getElementsByTagName("a")[document.getElementsByTagName("a").length - 2]
-
-let p1 = document.createElement('p')
-let textoValor = document.createTextNode(penultimo.getAttribute('href'))
-p1.appendChild(textoValor);
-document.getElementById('num-enlaces').appendChild(p1);
+document.getElementById('num-enlaces').innerHTML = enlaces ;
+let divEjercicios = document.getElementById('num-enlaces');
+let enlacesArray = document.getElementsByTagName('a');
 
 
+let penultimoEnlace = enlacesArray[enlaces-2].getAttribute('href');
+
+let p1 = document.createElement('p');
+let texto1 = document.createTextNode(penultimoEnlace);
+p1.appendChild(texto1);
+divEjercicios.appendChild(p1);  // null, no tiene direccion
 
 
-let cont = 0;
 
-for (let i = 0; i < document.getElementsByTagName("a").length; i++) {
-
-  if (document.getElementsByTagName("a")[i] == 'http://prueba') cont++;
-
+let contador = 0;
+for(let i = 0; i < enlacesArray.length; i++){
+  if(enlacesArray[i] == 'http://prueba'){
+    contador++
+  }
 }
-let p2 = document.createElement('p')
-let texto2 = document.createTextNode(cont)
-p1.appendChild(texto2);
-document.getElementById('num-enlaces').appendChild(p2);
+let p2 = document.createElement('p');
+let texto2 = document.createTextNode('Enlaces a "http://prueba": ' +contador); // 0, ninungo enlaza a esa direcion
+p2.appendChild(texto2);
+divEjercicios.appendChild(p2);
 
-let p3 = document.getElementsByTagName('p').length;
 
-if (p3 >= 3) {
 
-  p3 = document.getElementsByTagName('p')[2].getElementsByTagName('a').length;
+let parrafos = document.getElementsByTagName('p');
+let enlacesParrafo3 = document.getElementsByTagName('p')[2].getElementsByTagName('a').length;
+console.log('Numero de enlaces en el 3er parrafo: '+ enlacesParrafo3);
 
-}
 
-console.log(p3);
+
