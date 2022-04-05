bfb4a73093e1b747c178b955fdb4fb1e73209921
Alejandro Quiñonez <alquinonez.dwfc5@ucamp.utel.edu.mx>
2022-04-04 22:36:13 -0600

    ejercicio realizado

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
 let hola = document.getElementById('hola');
 hola.innerHTML = 'Hola Bienvenido a la manipulacion del DOM';
 
-// definimos el boton de guardar con el elemento buscando por id
-const botonGuardar = document.getElementById('guardar')
-// definimos el inputValue buscando por su id 
-const inputValue = document.querySelector('#input-1')
-// definimos un array vacio
-let arr = []
 
-// creamos un add eventListener que escuchara el click del boton guardad
-botonGuardar.addEventListener('click', () => {
-  // agrega el valor del input al final del arreglo arr
-  arr.push(inputValue.value)
-  // lo guardamos en localStorage con la llave lista-pokemon y el valor es el arrego arr
-  localStorage.setItem('lista-pokemon', arr)
-  //imprimimos en consola el arreglo actualizado
-  console.log(arr)
-  //limpiamos el valor del input
-  inputValue.value = ''
-  renderElement(arr)
+// Definimos el boton de guardar con el elemento buscado por id
+const btnGuardar = document.getElementById('guardar');
+// Definimos el input value buscando por id con query selector
+const inputValue = document.querySelector('#input-1');
+// Definimos un array vacio;
+let arr = [];
+
+//Creamos un Listener que escuchara el click del boton guardar.
+btnGuardar.addEventListener('click', ()=>{
+  // agrega el valor del input al array
+  arr.push(inputValue.value);
+  // lo guardamos en local storage con la llave lista pokemon y el valor es el arreglo arr
+  localStorage.setItem('lista-pokemon', arr);
+  // Imprimir en consola el arreglo actualizado
+  console.log(arr);
+  // limpiamos el valor del input
+  inputValue.value = '';
+
+  renderElement(arr);
 })
 
-function renderElement() {
-  let container = document.getElementById('lista')
-  console.log(arr)
+
+function renderElement(){
+  let container = document.getElementById('lista');
   container.innerHTML = '';
-  arr.forEach(function (el) {
+  arr.forEach((el)=>{
     container.innerHTML += `
-    <li>
-      ${el}
-    </li>
+      <li>${el}</li>
   `;
-  })
+  });
+
 }
 
 
-function getElements() {
-  let localS = localStorage.getItem('lista-pokemon')
-  arr = localS.split(',')
-  renderElement()
+function getElements(){
+  let localS = localStorage.getItem('lista-pokemon');
+
+   Array = localS.split(',');
 }
-getElements()
 
-
-
-
+getElements();
